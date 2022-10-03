e8cc0d9daf02480262a104af8f9c44793af6e792
ZecaSantos <zecadeveloper@gmail.com>
2022-10-03 17:07:24 -0300

    Primeira autalização

--- src/App.java
+++ src/App.java
@@ -7,8 +7,9 @@ public class App {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-       int N, id, i, qtd_man, qtd_woman, total_alturas_woman;
-       String nome, sexo, atl_alto_mais;
+       int N, id, qtd_man, qtd_woman, total_alturas_woman;
+       String nome, atl_alto_mais;
+       char sexo;
        double altura, peso, pes_medio, peso_total, porcent_man, alt_media_woman, 
        nova_altura, cadastro;
 
@@ -17,9 +18,18 @@ public class App {
        System.out.println("Qual é a quantidade de atletas? ");
        N = sc.nextInt();
 
-       for(int i == 0; i ==1 ; i == n){
+       for(int i = 1; i < N; i++){
+            System.out.printf("Digite os dados do atleta numero", i);
+            System.out.println("Nome: ");
+            nome = sc.next();
 
-       }
+            System.out.println();
+            System.out.println("Sexo (F / M):");
+            sexo = sc.next().charAt(0);
+
+               do{
+                    
+               }
 
 
 
