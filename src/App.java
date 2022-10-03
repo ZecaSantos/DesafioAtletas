d943cc9acfe87096233e9283d62864592206ff1b
ZecaSantos <zecadeveloper@gmail.com>
2022-10-03 16:48:53 -0300

    Lançamento

---
+++ src/App.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+       int N, id, i, qtd_man, qtd_woman, total_alturas_woman;
+       String nome, sexo, atl_alto_mais;
+       double altura, peso, pes_medio, peso_total, porcent_man, alt_media_woman, 
+       nova_altura, cadastro;
+
+       alt_media_woman = 1;
+
+       System.out.println("Qual é a quantidade de atletas? ");
+       N = sc.nextInt();
+
+       for(int i == 0; i ==1 ; i == n){
+
+       }
+
+
+
+    sc.close(); 
+    }
+}
